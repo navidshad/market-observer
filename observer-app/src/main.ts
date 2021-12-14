c117ea8564932bf23374d11385be77fbcb6dfc81
Navid Shad <navidshad72@gmail.com>
2021-12-14 22:46:14 +0300

    web - add bootstrap and tailwind

--- observer-app/src/main.ts
+++ observer-app/src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import bootstrap from './plugins/bootstrap'
+
+import './assets/tailwind.css'
+
+
+
+createApp(App)
+	.use(store)
+	.use(router)
+	.use(bootstrap)
+	.mount('#app')
---
+++ observer-app/src/views/UpdateAllCoins.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex justify-center items-center"
+    :style="{ width: '100vw', height: '100vh' }"
+  >
+    <div class="w-64">
+      <h3>Before start trading you have to update all coins data.</h3>
+      <br>
+      <b-button>Update</b-button>
+      <b-button variant="danger">Button</b-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class UpdateAllCoins extends Vue {}
+</script>
